53e210f2939d1fe1335f699c5232c1f7ffd2ae74
leftleftleft <93295466+leftleftleft@users.noreply.github.com>
2022-01-17 11:55:08 -0800

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,6 +1,6 @@
 # Just a demo py app that prints secrets
 
-secret = "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"
+secret = "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"
 
 def main():
   print("Here is your secret:", secret)
